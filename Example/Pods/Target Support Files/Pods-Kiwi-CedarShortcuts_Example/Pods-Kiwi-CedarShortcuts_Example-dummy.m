5f2b40136faec781c3c81b851f76f4cab730a1bb
Paul Zabelin <paulz@users.noreply.github.com>
2015-11-14 13:38:22 -0800

    remove example app, use tests only

--- Example/Pods/Target Support Files/Pods-Kiwi-CedarShortcuts_Example/Pods-Kiwi-CedarShortcuts_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Kiwi_CedarShortcuts_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Kiwi_CedarShortcuts_Example
-@end
--- Example/Pods/Target Support Files/Pods-Kiwi-CedarShortcuts_Example/Pods-Kiwi-CedarShortcuts_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Kiwi_CedarShortcuts_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Kiwi_CedarShortcuts_ExampleVersionString[];
-
